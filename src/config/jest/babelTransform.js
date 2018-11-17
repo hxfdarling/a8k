b79d22966116ed81c6fe8a852bccdb56219301c6
zman <hxfdarling@hotmail.com>
2018-11-17 17:26:23 +0800

    refactor: 删除部分注释

--- src/config/jest/babelTransform.js
+++ src/config/jest/babelTransform.js
@@ -1,11 +1,3 @@
-// @remove-file-on-eject
-/**
- * Copyright (c) 2014-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
--- src/config/jest/cssTransform.js
+++ src/config/jest/cssTransform.js
@@ -1,12 +1,3 @@
-// @remove-on-eject-begin
-/**
- * Copyright (c) 2014-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-// @remove-on-eject-end
-
 module.exports = {
   process() {
     return 'module.exports = {};';
