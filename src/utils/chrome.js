bf650eee0dff69fcd3d7a298a11317ea63fbd123
edenhliu <edenhliu@tencent.com>
2018-11-20 13:19:39 +0800

    fix: 修复windows获取chrome异常

--- src/utils/chrome.js
+++ src/utils/chrome.js
@@ -1,63 +1,12 @@
 const puppeteer = require('puppeteer-core');
-const commandExists = require('command-exists');
-
-async function isCommandExists(cmd) {
-  return new Promise((resolve, reject) => {
-    commandExists(cmd, (err, exists) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(exists);
-      }
-    });
-  });
-}
-
-async function detectChrome() {
-  if (await isCommandExists('google-chrome-unstable')) {
-    return 'google-chrome-unstable';
-  }
-  if (await isCommandExists('google-chrome-beta')) {
-    return 'google-chrome-beta';
-  }
-  if (await isCommandExists('google-stable')) {
-    return 'google-stable';
-  }
-  if (await isCommandExists('google-chrome')) {
-    return 'google-chrome';
-  }
-  if (await isCommandExists('chromium')) {
-    return 'chromium';
-  }
-  if (await isCommandExists('chromium-browser')) {
-    return 'chromium-browser';
-  }
-  // windows
-  if (await isCommandExists('chrome')) {
-    return 'chrome';
-  }
-  // macos
-  if (await isCommandExists('/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome')) {
-    return '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome';
-  }
-  if (await isCommandExists('/Applications/Google Chrome Dev.app/Contents/MacOS/Google Chrome')) {
-    return '/Applications/Google Chrome Dev.app/Contents/MacOS/Google Chrome';
-  }
-  if (await isCommandExists('/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome')) {
-    return '/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome';
-  }
-  if (await isCommandExists('/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')) {
-    return '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-  }
-  throw Error("Couldn't detect chrome version installed! use --chrome-binary to pass custom location");
-}
+const findChrome = require('chrome-finder');
 
 process.on('unhandledRejection', err => {
   throw err;
 });
 
 exports.launch = async (options = {}) => {
-  const executablePath = await detectChrome();
+  const executablePath = findChrome();
   const browser = await puppeteer.launch({
     executablePath,
     ...options,
