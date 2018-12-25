df20c9d2e9c45557cf91c3655a5c50be3f662cf9
edenhliu <edenhliu@tencent.com>
2018-12-25 20:05:33 +0800

    feat: 添加node_modules缓存能力

--- bin/cli.js
+++ bin/cli.js
@@ -13,6 +13,7 @@ const getOptions = require('../src/utils/getOptions');
 const spinner = require('../src/utils/spinner');
 const { logWithSpinner, stopSpinner } = require('../src/utils/spinner');
 const { error } = require('../src/utils/logger');
+const cache = require('../src/scripts/cache');
 
 const cwd = process.cwd();
 
@@ -91,6 +92,13 @@ program
     const options = getOptions({});
     await fs.emptyDir(options.cache);
     spinner.stopSpinner();
+  });
+program
+  .command('cache <cmd>')
+  .description('缓存node_modules加速构建,使用方法：imt cache [cmd]. cmd 是配置的 npm scripts key')
+  .action(async cmd => {
+    const options = getOptions({});
+    cache({ cmd, cache: options.cache });
   });
 const initChoices = [
   { name: '添加 提交前 lint 和 prettier', value: 'lint' },
