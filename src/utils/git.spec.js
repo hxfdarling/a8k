d19824ff263086bb2a4c037cd1c58cb3376b1941
zman <hxfdarling@hotmail.com>
2019-01-10 00:43:22 +0800

    test: 添加测试用例

--- src/utils/git.spec.js
+++ src/utils/git.spec.js
@@ -1,1 +1,10 @@
-test('获取分支名称', async () => {});
+const git = require('./git.js');
+
+test('是Git仓库', async () => {
+  // travis 不运行该测试用例
+  if (process.env.CI_ENV === 'travis') {
+    return;
+  }
+  const flag = await git.isGitProject();
+  expect(flag).toBe(true);
+});
