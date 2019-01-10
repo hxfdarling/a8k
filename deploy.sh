0636242c131bac2d5dbb300cbc8f1561523c7767
edenhliu <edenhliu@tencent.com>
2019-01-10 15:34:21 +0800

    docs: 添加文档支持

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+
+git init
+git add -A
+git commit -m 'chore: deploy'
+
+
+git push -f git@github.com:hxfarling/imt.git master:gh-pages
+
+cd -
