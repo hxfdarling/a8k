aecca230889bec117d2bcff8e3db28e7d4bc7a2a
zman <hxfdarling@hotmail.com>
2019-01-19 11:58:56 +0800

    test: 添加测试用例

--- src/utils/git.spec.js
+++ src/utils/git.spec.js
@@ -1,1 +1,13 @@
-test('获取分支名称', async () => {});
+import 'jest-extended';
+import { resolve } from 'path';
+import { getBranchName, isGitProject, getBranches } from './git';
+
+test('获取分支名称', async () => {
+  expect(await getBranchName(resolve('.'))).toBeString();
+});
+test('判断是否是git仓库', async () => {
+  expect(await isGitProject(resolve('.'))).toBeTrue();
+});
+test('获取所有分支名称', async () => {
+  expect(await getBranches(resolve('.'))).toBeArray();
+});
