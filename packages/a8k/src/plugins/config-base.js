4358793898842d8fd036060948c2cec475bf3fbd
edenhliu <edenhliu@tencent.com>
2019-03-20 11:54:34 +0800

    fix: 修复项目中使用了unknow require语法，导致无法正确查找依赖

--- packages/a8k/src/plugins/config-base.js
+++ packages/a8k/src/plugins/config-base.js
@@ -71,7 +71,8 @@ exports.apply = context => {
     // 这些库都是不依赖其它库的库 不需要解析他们可以加快编译速度
     config.module.noParse(/node_modules\/(moment)/);
     // 动态依赖默认不处理子目录，仅处理单层目录
-    config.module.set('wrappedContextRecursive', false);
+    // TODO：项目里面有使用到
+    // config.module.set('wrappedContextRecursive', false);
 
     require('../webpack/rules/js')(config, context, options);
     require('../webpack/rules/css')(config, context, options, filenames.css);
