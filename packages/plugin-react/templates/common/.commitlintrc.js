63b9926bb0033fd9c05f4021e2eddc0248936555
edenhliu <edenhliu@tencent.com>
2019-05-23 12:06:23 +0800

    refactor: 更新模板项目

--- packages/plugin-react/templates/common/.commitlintrc.js
+++ packages/plugin-react/templates/common/.commitlintrc.js
@@ -1,6 +1,6 @@
 // 校验commit消息是否符合规范
 module.exports = {
-  extends: ['imt', 'cz'],
+  extends: ['@a8k/changelog', 'cz'],
   rules: {
     // 自定义检测配置
   },
--- packages/plugin-react/templates/common/a8k.config.js
+++ packages/plugin-react/templates/common/a8k.config.js
@@ -21,24 +21,13 @@ module.exports = {
     // host:'localhost',// 默认值
     port: 3000,  // 必须填写
   },
-  // pages目录下需要忽略的文件夹（不作为页面处理）
-  ignorePages: ['action_creators', 'action_types', 'reducers'],
   <% if(retry) { %>
   // 主域重试，有该值表示开启
   retry: {
     // 重试的根地址，和publicPath对应，例如://fudao.qq.com/pc/
     retryPublicPath: '',
-    // 排除非我们自己控制的JS
+    // 排除非自己控制的script链接
     exclude: [],
-    // 下面这些上报点，更具需要填写，如果需要统计重试的情况，请填写monitor上面申请的点
-    JS_SUCC_MSID: '', // 成功上报点
-    JS_FAIL_MSID: '', // 失败上报点
-    CSS_SUCC_MSID: '', // CSS成功
-    CSS_FAIL_MSID: '', // CSS失败
-    JS_RETRY_SUCC_MSID: '', // JS重试成功
-    JS_RETRY_FAIL_MSID: '', // JS重试失败
-    CSS_RETRY_SUCC_MSID: '', // CSS重试成功
-    CSS_RETRY_FAIL_MSID: '', // CSS重试失败
   },
   <% } %>
   // 配置额外的入口文件，会在每一个page中引用
@@ -58,9 +47,24 @@ module.exports = {
     },
   },
   <% } %>
-  // 修改webpack配置文件
-  chainWebpack() {
-
+   // 修改webpack配置文件
+   chainWebpack(config, { type, mode }) {
+    if (type === 'client') {
+      // 前端代码
+      if (mode === 'production') {
+        // 生产模式
+      } else if (mode === 'development') {
+        // 开发模式
+      }
+    } else if (type === 'server') {
+      // SSR代码
+      if (mode === 'production') {
+        // 生产模式
+      } else if (mode === 'development') {
+        // 开发模式
+      }
+    }
   },
+  // 添加a8k插件
   plugins: [],
 };
