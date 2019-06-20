860859f92dffb7bbd7a9de76968a000b7dcd2f79
liuhu <hxfdarling@hotmail.com>
2019-06-20 10:17:41 +0800

    fix: 修复ssr模板问题

--- packages/plugin-react-template/templates/multi/server/index.js
+++ packages/plugin-react-template/templates/multi/server/index.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const path = require('path');
-const ssr = require('@a8k/ssr-koa-middleware').default;
+const ssr = require('@a8k/ssr-koa-middleware');
 const {
   ssrDevServer: { port },
 } = require('../a8k.config.js');
