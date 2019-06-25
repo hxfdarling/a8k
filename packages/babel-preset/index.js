dc0ea8cdd2d6aa1c08d2cb679f99ed595b8dc694
eden <hxfdarling@hotmail.com>
2019-06-26 00:47:23 +0800

    refactor: html-loader use @a8k/babel-preset

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -1,6 +1,9 @@
+const validateOptions = require('schema-utils');
 const create = require('./create');
+const schema = require('./options.json');
 
 module.exports = function(api, options = {}) {
   const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-  return create(api, options, env);
+  validateOptions(schema, options, '@a8k/babel-preset');
+  return create(api, { useBuiltIns: 'usage', ...options }, env);
 };
